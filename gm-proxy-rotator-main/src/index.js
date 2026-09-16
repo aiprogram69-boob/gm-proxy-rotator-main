0eccb03961c735f44a200931c64e34942826e307
aiprogram69-boob <aiprogram69@gmail.com>
2026-09-16 16:22:31 +0300

    Delete gm-proxy-rotator-main directory

--- gm-proxy-rotator-main/src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    
-    const typliResponse = await env.TYPLI_API.fetch(request);
-    
-    // Возвращаем ответ боту
-    return typliResponse;
-  }
-}
